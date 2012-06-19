df8d5adce421f1851ea52abacb63c9f3a0c75599
Xavier Noria <fxn@hashref.com>
2012-06-19 08:58:44 +0200

    cheat-cheats update

--- cheat-sheets/git.sh
+++ cheat-sheets/git.sh
@@ -26,7 +26,7 @@ git config --global color.ui auto
 git config --global color.diff.whitespace "red reverse"
 git config --global merge.tool opendiff
 git config --global pull.rebase true
-
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
 
 # gitignore
 echo .idea > ~/.gitignore
